6f49d24e0f7edebde3092759f83341aa47efe2fd
marks_ubuntu_vm <1378539560@qq.com>
2022-07-20 19:47:04 +0800

    1st

---
+++ ch2/build/CMakeFiles/useHello.dir/useHello.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/useHello.dir/useHello.cpp.o: \
+ /home/marks/slambook2/ch2/useHello.cpp /usr/include/stdc-predef.h \
+ /home/marks/slambook2/ch2/libHelloSLAM.h
---
+++ ch2/libHelloSLAM.h
@@ -0,0 +1,8 @@
+#ifndef LIBHELLOSLAME_H_
+#define LIBHELLOSLAME_H_
+
+
+void printHello();
+
+
+#endif
